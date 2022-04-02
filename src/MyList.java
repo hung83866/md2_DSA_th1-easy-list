3ff25e1709185cccbbf11fbc3acfd9502cf8724b
unknown <hung.pnpt93@gmail.com>
2022-04-02 15:23:55 +0700

    ARRAYLIST

--- src/MyList.java
+++ src/MyList.java
@@ -1,27 +1,126 @@
 import java.util.Arrays;
 
-public class MyList<E>{
-    private int size = 0;
+public class MyList<E> {
+    private int size;
     private static final int DEFAULT_CAPACITY = 10;
-    private Object elements[];
+    private E elements[];
+
     public MyList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
     }
+
     private void ensureCapa() {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public E[] getElements() {
+        return elements;
+    }
+
+    public void setElements(E[] elements) {
+        this.elements = elements;
+    }
+
     public void add(E e) {
         if (size == elements.length) {
             ensureCapa();
         }
         elements[size++] = e;
     }
+
     public E get(int i) {
-        if (i>= size || i <0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
         }
         return (E) elements[i];
     }
+
+    public void add(int index, E e) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        } else {
+            size++;
+            if (size == elements.length) {
+                ensureCapa();
+            }
+            for (; index < size; index += 2) {
+                E temp = elements[index];
+                elements[index] = e;
+                e = elements[index + 1];
+                elements[index + 1] = temp;
+            }
+        }
+    }
+
+    public E remove(int index) {
+        E removeValue = null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index not found");
+        } else {
+            removeValue = elements[index];
+            for (; index <size ; index++) {
+              elements[index]= elements[index+1];
+            }
+            size--;
+        }
+        return removeValue;
+    }
+
+    public int size() {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] != null)
+                size++;
+        }
+        return size;
+    }
+
+    public MyList<E> clone() {
+        MyList<E> list = new MyList<>();
+        for (int i = 0; i < size; i++) {
+            list.add((E) elements[i]);
+        }
+        return list;
+    }
+
+    public boolean contains(E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(E o) {
+        int index = -1;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                index = i;
+            }
+        }
+        if (index == -1 ){
+            System.out.println("không có trong mảng");
+        }
+        return index;
+    }
+
+    public void render() {
+        for (int i = 0; i < this.getSize(); i++) {
+            System.out.println(this.get(i));
+        }
+    }
+    public void Clear(){
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
 }
+
 
--- src/MyListTest.java
+++ src/MyListTest.java
@@ -1,16 +1,40 @@
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<Integer> listInteger = new MyList<Integer>();
+        MyList<Integer> listInteger = new MyList<>();
         listInteger.add(1);
         listInteger.add(2);
         listInteger.add(3);
         listInteger.add(4);
-        listInteger.add(5);
-        System.out.println("(4) = "+listInteger.get(4));
+        System.out.println("(4) = "+listInteger.get(3));
         System.out.println("(1) = "+listInteger.get(1));
         System.out.println("(0) = "+listInteger.get(0));
         listInteger.add(23);
         listInteger.add(34);
-        System.out.println("(-1)="+listInteger.get(-1));
+        System.out.println("________________Check add phần tử vào cuối________________");
+        System.out.println(listInteger.getSize());
+        listInteger.render();
+        System.out.println("________________add phần tử vào giữa________________");
+
+        listInteger.add(3,15);
+        listInteger.render();
+        System.out.println("_________________remove phần tử_______________");
+        System.out.println("remove element ====="+listInteger.remove(3));;
+        listInteger.render();
+        System.out.println("________________Clone mảng________________");
+        MyList<Integer> array = new MyList<Integer>();
+        array = listInteger.clone();
+        array.render();
+        System.out.println("________________boolean contain tìm phần tử trong mảng________________");
+        System.out.println(array.contains(10));
+        System.out.println("________________tìm vitri phần tử________________");
+        System.out.println("vị trí = "+array.indexOf(10));
+        System.out.println("vị trí = "+array.indexOf(3));
+
+        System.out.println("_______________Clear test_________________");
+        array.render();
+        array.Clear();
+        System.out.println("sau khi clearrrr = ");
+        array.render();
+        System.out.println("________________OK________________");
     }
 }
